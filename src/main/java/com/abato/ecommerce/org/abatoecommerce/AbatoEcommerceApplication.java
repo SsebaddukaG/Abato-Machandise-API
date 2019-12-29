b05c2e46e1ac9ef4511902fd77dffb587673856d
SsebaddukaG <luckygilbert1993@gmail.com>
2019-12-29 17:51:21 -0600

    project setup

---
+++ src/main/java/com/abato/ecommerce/org/abatoecommerce/AbatoEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.abato.ecommerce.org.abatoecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbatoEcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbatoEcommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/abato/ecommerce/org/abatoecommerce/AbatoEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abato.ecommerce.org.abatoecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbatoEcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
