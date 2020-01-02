4b6b5a77fe19257554316859a67e91cf0022d61d
SsebaddukaG <luckygilbert1993@gmail.com>
2020-01-02 16:56:58 -0600

    Created addCategory method with tests inclusive

--- src/test/java/com/abato/ecommerce/org/abatoecommerce/AbatoEcommerceApplicationTests.java
+++ src/test/java/com/abato/ecommerce/org/abatoecommerce/AbatoEcommerceApplicationTests.java
@@ -1,13 +1,36 @@
 package com.abato.ecommerce.org.abatoecommerce;
 
+import com.abato.ecommerce.org.abatoecommerce.dao.ICategoryRepository;
+import com.abato.ecommerce.org.abatoecommerce.model.ProductCategory;
+import com.abato.ecommerce.org.abatoecommerce.services.CategoryService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class AbatoEcommerceApplicationTests {
+    @Autowired
+    private CategoryService service;
+
+    @MockBean
+    private ICategoryRepository repository;
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    public void addCategory() {
+        ProductCategory category = new ProductCategory();
+        category.setCategoryName("Abato");
+        category.setcId(1);
+
+        when(repository.save(category)).thenReturn(category);
+        assertEquals(category,service.addCategory(category));
     }
 
 }
